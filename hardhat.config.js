7808a660cffacc7bdff5c14158177310738cb002
DivyanshGoel20 <divyanshgoel17@gmail.com>
2024-11-03 16:23:00 +0530

    add: smart contract and front-end files

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,22 @@
-require("@nomicfoundation/hardhat-toolbox");
+/**
+* @type import('hardhat/config').HardhatUserConfig
+*/
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+
+const { API_URL, PRIVATE_KEY } = process.env;
+
 module.exports = {
-  solidity: "0.8.27",
+   solidity: "0.8.11",
+   defaultNetwork: "aiaTestnet",
+   networks: {
+      hardhat: {},
+      aiaTestnet: {
+         url: "https://1320.rpc.thirdweb.com",
+         chainId: 1320,
+         accounts: [`0x${PRIVATE_KEY}`],
+         gasPrice: "auto",
+      },
+   },
 };
